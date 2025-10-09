14e2113b7840e76f5689367e8179124b371d473d
sasegura <sasegura.fernandez87@gmail.com>
2025-10-09 16:31:38 +0200

    git deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true, 
+  basePath: '/portfolio', 
+  assetPrefix: '/portfolio',
   images: {
     unoptimized: true,
     remotePatterns: [
