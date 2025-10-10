66ae8053b2ad99b76af05c1b7a9b6a79c2fcc35b
sasegura <sasegura.fernandez87@gmail.com>
2025-10-10 09:46:57 +0200

    Github action update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const isExport = process.env.EXPORT === 'true';
+
 const nextConfig = {
-  output: 'export',
+  ...(isExport && { output: 'export' }),
   trailingSlash: true, 
-  basePath: '/portfolio', 
-  assetPrefix: '/portfolio',
+  ...(isExport && { 
+    basePath: '/devportfolio', 
+    assetPrefix: '/devportfolio/',
+  }),
   images: {
     unoptimized: true,
     remotePatterns: [
